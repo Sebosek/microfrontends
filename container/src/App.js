a741ea6feb0040184a7ccc184852db9c01864115
Sebastian Busek <ja@sebastianbusek.cz>
2021-02-26 23:28:00 +0100

    feat(): added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,10 +1,14 @@
 ﻿import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
 
 export default () => (
-  <>
-    <h1>Container</h1>
-    <hr />
-    <MarketingApp />
-  </>
+  <BrowserRouter>
+    <div>
+      <Header />
+      <MarketingApp />
+    </div>
+  </BrowserRouter>
 );
